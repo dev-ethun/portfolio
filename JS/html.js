a8a1b1183d6be90215c680c73c52af753f7ea304
Dev-Ethun <ethuntcg5@gmail.com>
2025-01-13 19:18:07 +0600

    update

---
+++ JS/html.js
@@ -0,0 +1,43 @@
+
+
+function intro(){
+    window.location.href='../Html/intro.html'
+}
+
+function editor(){
+    window.location.href='../Html/editor.html'
+}
+
+function home(){
+    window.location.href='../Html/core.html'
+}
+
+function basics(){
+    window.location.href='../Html/basics.html'
+}
+
+function previous1(){
+    window.location.href='../Html/intro.html'
+}
+
+function back(){
+    window.location.href='../Html/course.html'
+}
+
+function download(){
+        window.location.href='https://play.google.com/store/apps/details?id=com.qamar.editor.html'
+}
+
+function downloadPc(){
+        window.location.href='https://code.visualstudio.com/download'
+}
+
+document.getElementById("doc").textContent =' <!DOCTYPE html>  ';
+document.getElementById("doc2").textContent =' <html>  ';
+document.getElementById("doc3").textContent =' <head>  ';
+document.getElementById("doc4").textContent =' <title>  ';
+document.getElementById("doc5").textContent =' <body>  ';
+document.getElementById("doc6").textContent =' <h1>  ';
+document.getElementById("doc7").textContent =' <p>  ';
+
+
---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function hey(){
+    window.location.href='../Html/course.html'
+}
+
+function core(){
+      window.location.href='../Html/core.html'
+}
